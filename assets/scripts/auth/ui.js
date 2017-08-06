f57073ce79a2dd03405923e1744443a0d175a93b
jbculbertson <jbculbertson@gmail.com>
2017-08-06 13:08:03 -0400

    Add modal hide and val clear for auth
    - CP
    - signup
    - signin

--- assets/scripts/auth/ui.js
+++ assets/scripts/auth/ui.js
@@ -9,6 +9,10 @@ const signInSuccess = (data) => {
   store.user = data.user
   console.log('Sign In Success!')
   console.log(data.user)
+  $('#sign-up-modal').modal('hide')
+  $('.sign-up').val('')
+  $('#sign-in-modal').modal('hide')
+  $('.sign-in').val('')
 }
 
 const signInFailure = () => {
@@ -17,6 +21,8 @@ const signInFailure = () => {
 const changePasswordSuccess = (data) => {
   console.log('Change Password Success!')
   console.log(data)
+  $('#change-password-modal').modal('hide')
+  $('.change-password').val('')
 }
 
 const changePasswordFailure = () => {
