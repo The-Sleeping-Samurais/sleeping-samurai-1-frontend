53017730222ecd218426812c7f99fba1c24a25b3
coreyfedde <coreysfedde@gmail.com>
2017-08-06 14:59:37 -0400

    Creates test branch to see uploaded files
    - Adds handlebar template to display info on uploaded file success

--- assets/scripts/uploads/ui.js
+++ assets/scripts/uploads/ui.js
@@ -1,8 +1,11 @@
 'use strict'
 const store = require('../store')
+const uploadTemplate = require('../templates/show-upload.handlebars')
 
 const success = function (data) {
   console.log('success data is:', data)
+  const uploadHTML = uploadTemplate({ upload: data.upload })
+  $('#uploadsBoard').append(uploadHTML)
 }
 
 const error = function (error) {
